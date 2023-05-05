a1d4ffafbab442856126b3c4a668031075d2843f
Muhammad Tahir <81794146+Tahir187@users.noreply.github.com>
2023-05-05 09:53:39 +0500

    added functionality to counter buttons

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
+import {incNumber, decNumber} from "./actions/index"
 
 function App() {
   const myState = useSelector((state) => state.changeTheNumber)
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>React Redux</h1>
@@ -11,8 +13,8 @@ function App() {
       <br></br>
       <br></br>
       <h2>{myState}</h2>
-      <button>Increment</button>
-      <button>Decrement</button>
+      <button onClick={()=> dispatch(incNumber())}>Increment</button>
+      <button onClick={()=> dispatch(decNumber())}>Decrement</button>
     </div>
   );
 }
